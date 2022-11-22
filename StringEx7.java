c8af7951fec57b24d1c7bde373bfb579a3b8da19
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2022-11-22 23:11:27 +0530

    Working one check it once

--- StringEx7.java
+++ StringEx7.java
@@ -2,17 +2,20 @@ package com.practice.stringAssignment;
 
 public class StringEx7 {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		String str = "bvrit college";
-		char ch = 'l';
-		int i = str.indexOf(ch, str.indexOf(ch)+1);
-		System.out.println(str.indexOf(ch)+1);
-	//	System.out.println(str.toUpperCase().charAt(i));
-		//System.out.println(str.charAt(i));
-		System.out.println(str.replace(str.charAt(str.indexOf(ch)+1), str.toUpperCase().charAt(i)));
-		
-		
-		
+		char ch[] = str.toCharArray();
+		int j = str.lastIndexOf("l");
+		String str1 = null;
+		for (int i = 0; i < ch.length; i++)
+
+		{
+			if (j == i) {
+				ch[j] = Character.toUpperCase(ch[i]);
+			}
+			str1 = new String(ch);
+		}
+		System.out.println(str1);
 	}
 
 }
